73d6266b723942dfbce66aee5abb772c379c3937
Greg Harner <accounts@yongsa.net>
2024-04-25 10:38:40 -0400

    add shared contact

--- src/routes/gapi.ts
+++ src/routes/gapi.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { accessToken, addMember, directory, events, googleLogin, group, members, oAuthCallback, removeMember } from '../controller';
+import { accessToken, addMember, directory, events, googleLogin, group, members, oAuthCallback, removeMember, sharedContact } from '../controller';
 import { IRoutes } from '../interfaces';
 
 const router = Router();
@@ -13,7 +13,7 @@ router.get('/group', group);
 router.get('/members', members);
 router.get('/oAuthCallback', oAuthCallback);
 router.get('/removeMember', removeMember);
-
+router.get('/sharedContact', sharedContact);
 export const gapi: IRoutes = {
 	name: 'gapi',
 	router,
