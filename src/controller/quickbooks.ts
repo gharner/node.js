7b1d54ec625332ede016b3a2ee611092a19c5fdd
Greg Harner <accounts@yongsa.net>
2024-05-17 11:01:09 -0400

    quickbooks api

--- src/controller/quickbooks.ts
+++ src/controller/quickbooks.ts
@@ -1,125 +1,150 @@
+import axios, { AxiosResponse } from 'axios';
 import { Request, Response } from 'express';
-import { admin } from '../middleware/firebase';
+import { logger } from 'firebase-functions/v1';
 import OAuthClient from 'intuit-oauth';
+import { qbToken } from '../interfaces';
+import { admin } from '../middleware/firebase';
 import qbDev from '../middleware/quickbooks.dev.json';
 import qbProd from '../middleware/quickbooks.prod.json';
-import { qbToken } from '../interfaces';
-//import { logger } from 'firebase-functions';
 
-const config: any = process.env.GCLOUD_PROJECT === 'mas-development-53ac7' ? qbDev : qbProd;
+const config = process.env.GCLOUD_PROJECT === 'mas-development-53ac7' ? qbDev : qbProd;
 
 const oauthClient = new OAuthClient({
 	clientId: config.client_id,
 	clientSecret: config.client_secret,
-	environment: 'sandbox',
+	environment: config.state === 'development' ? 'sandbox' : 'production',
 	redirectUri: config.redirect_uri,
 });
 
-export const auth_token = (request: Request, response: Response) => {
-	const parseRedirect = request.url;
-
-	// Exchange the auth code retrieved from the **req.url** on the redirectUri
-	oauthClient
-		.createToken(parseRedirect)
-		.then(authResponse => {
-			const payload = authResponse.getJson();
-			return payload as unknown as qbToken;
-		})
-		.then(payload => {
-			payload.server_time = new Date().valueOf();
-
-			const t1 = new Date();
-			t1.setSeconds(t1.getSeconds() + payload.expires_in);
-			payload.expires_time = t1.valueOf();
-
-			const t2 = new Date();
-			t2.setSeconds(t2.getSeconds() + payload.x_refresh_token_expires_in);
-			payload.refresh_time = t2.valueOf();
-
-			admin.firestore()
-				.doc('/mas-parameters/quickbooksAPI')
-				.set(payload, { merge: true })
-				.then(() => {
-					const html_response =
-						'<!DOCTYPE html><html lang="en"><head><title>Quickbooks Token Response</title><script>window.close();</script></head><body><h4>New Token Issued</h4></body></html>';
-					response.send(html_response);
-				});
-		})
-		.catch(e => {
-			response.send(e);
-		});
+const handleError = (response: Response, error: unknown) => {
+	if (error instanceof Error) {
+		logger.error('Error:', error.message);
+		response.status(500).send({ error: error.message });
+	} else {
+		logger.error('Unknown error:', error);
+		response.status(500).send({ error: 'Internal Server Error' });
+	}
 };
 
-export const get_updates = (request: Request, response: Response) => {
-	const axios = require('axios');
-
-	admin.firestore()
-		.doc('/mas-parameters/quickbooksAPI')
-		.get()
-		.then(doc => {
-			const data = doc.data() as qbToken;
-			let lastUpdated: string = '';
-
-			if (data.expires_time) {
-				lastUpdated = new Date(data.lastCustomerUpdate).toISOString().substring(0, 10);
-
-				const query = `Select * from Customer where Metadata.LastUpdatedTime > '${lastUpdated}'`;
-
-				const config = {
-					method: 'get',
-					url: `https://${request.headers.endpoint}/v3/company/${request.headers.company}/query?query=${query}`,
-					headers: {
-						Accept: 'application/json',
-						'Content-Type': 'application/text',
-						Authorization: `Bearer ${request.headers.token}`,
-					},
-				};
-
-				axios(config)
-					.then((result: any) => {
-						response.send(result.data.QueryResponse);
-					})
-					.catch((error: any) => {
-						response.send(error);
-					});
-			} else {
-				response.send('token error');
-			}
-		});
+const isQbToken = (obj: any): obj is qbToken => {
+	return (
+		obj &&
+		typeof obj.access_token === 'string' &&
+		typeof obj.expires_in === 'number' &&
+		typeof obj.refresh_token === 'string' &&
+		typeof obj.x_refresh_token_expires_in === 'number' &&
+		typeof obj.lastCustomerUpdate === 'string'
+	);
 };
 
-export const getCustomerByEmail = (request: Request, response: Response) => {
-	const axios = require('axios');
-
-	const query = `Select * from Customer where PrimaryEmailAddr = '${request.headers.email}'`;
-
-	const config = {
-		method: 'get',
-		url: `https://${request.headers.endpoint}/v3/company/${request.headers.company}/query?query=${query}`,
-		headers: {
-			Accept: 'application/json',
-			'Content-Type': 'application/text',
-			Authorization: `Bearer ${request.headers.token}`,
-		},
-	};
-
-	axios(config)
-		.then((result: any) => {
-			response.send(result.data.QueryResponse);
-		})
-		.catch((error: any) => {
-			response.send(error);
+export const auth_request = (response: Response) => {
+	try {
+		const auth_url = oauthClient.authorizeUri({
+			scope: config.scope,
 		});
+
+		logger.info('auth_url:', auth_url);
+
+		response.send(auth_url);
+	} catch (e) {
+		handleError(response, e);
+	}
+};
+export const auth_token = async (request: Request, response: Response) => {
+	try {
+		logger.info('auth_token:', oauthClient);
+
+		const parseRedirect = request.url;
+		logger.info('parseRedirect:', parseRedirect);
+
+		const authResponse = await oauthClient.createToken(parseRedirect);
+		logger.info('authResponse:', authResponse);
+
+		const payload = authResponse.getJson();
+		logger.info('payload:', payload);
+
+		if (!isQbToken(payload)) {
+			throw new Error('Invalid token payload');
+		}
+
+		payload.server_time = new Date().valueOf();
+		const t1 = new Date();
+		t1.setSeconds(t1.getSeconds() + payload.expires_in);
+		payload.expires_time = t1.valueOf();
+		const t2 = new Date();
+		t2.setSeconds(t2.getSeconds() + payload.x_refresh_token_expires_in);
+		payload.refresh_time = t2.valueOf();
+
+		await admin.firestore().doc('/mas-parameters/quickbooksAPI').set(payload, { merge: true });
+
+		const html_response =
+			'<!DOCTYPE html><html lang="en"><head><title>Quickbooks Token Response</title><script>window.close();</script></head><body><h4>New Token Issued</h4></body></html>';
+		response.send(html_response);
+	} catch (e) {
+		handleError(response, e);
+	}
 };
 
-export const refresh_token = (request: Request, response: Response) => {
-	oauthClient
-		.refreshUsingToken(request.headers.refresh_token as string)
-		.then(function (authResponse) {
-			response.send(authResponse.getJson());
-		})
-		.catch(function (e) {
-			console.error('The error message is :' + e.originalMessage);
-			console.error(e.intuit_tid);
-		});
+export const get_updates = async (request: Request, response: Response) => {
+	try {
+		const doc = await admin.firestore().doc('/mas-parameters/quickbooksAPI').get();
+		const data = doc.data() as qbToken;
+
+		if (!data || !data.expires_time) {
+			response.status(400).send({ error: 'Invalid or expired token' });
+		}
+
+		const lastUpdated = new Date(data.lastCustomerUpdate).toISOString().substring(0, 10);
+		const query = `Select * from Customer where Metadata.LastUpdatedTime > '${lastUpdated}'`;
+
+		const config = {
+			method: 'get',
+			url: `https://${request.headers.endpoint}/v3/company/${request.headers.company}/query?query=${query}`,
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/text',
+				Authorization: `Bearer ${request.headers.token}`,
+			},
+		};
+
+		const result: AxiosResponse = await axios(config);
+		response.send(result.data.QueryResponse);
+	} catch (e) {
+		handleError(response, e);
+	}
+};
+
+export const getCustomerByEmail = async (request: Request, response: Response) => {
+	try {
+		const query = `Select * from Customer where PrimaryEmailAddr = '${request.headers.email}'`;
+
+		const config = {
+			method: 'get',
+			url: `https://${request.headers.endpoint}/v3/company/${request.headers.company}/query?query=${query}`,
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/text',
+				Authorization: `Bearer ${request.headers.token}`,
+			},
+		};
+
+		const result: AxiosResponse = await axios(config);
+		response.send(result.data.QueryResponse);
+	} catch (e) {
+		handleError(response, e);
+	}
+};
+
+export const refresh_token = async (request: Request, response: Response) => {
+	try {
+		const authResponse = await oauthClient.refreshUsingToken(request.headers.refresh_token as string);
+		response.send(authResponse.getJson());
+	} catch (e) {
+		if (e instanceof Error) {
+			console.error('The error message is:', e.message);
+		} else {
+			console.error('Unknown error:', e);
+		}
+		handleError(response, e);
+	}
 };
--- src/routes/quickbooks.ts
+++ src/routes/quickbooks.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { auth_token, get_updates, refresh_token, getCustomerByEmail } from '../controller';
+import { auth_token, get_updates, refresh_token, getCustomerByEmail, auth_request } from '../controller';
 import { IRoutes } from '../interfaces';
 
 const router = Router();
@@ -8,6 +8,7 @@ router.get('/auth_token', auth_token);
 router.get('/get_updates', get_updates);
 router.get('/refresh_token', refresh_token);
 router.get('/getCustomerByEmail', getCustomerByEmail);
+router.get('/auth_request', auth_request);
 
 export const qb: IRoutes = {
 	name: 'qb',
